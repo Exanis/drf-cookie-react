65feb78ca92349c097e4dda465858cb5a93b6049
Yann PIQUET <yann.piquet@inference-operations.com>
2017-12-07 11:28:20 +0100

    Update creation + add healthcheck

---
+++ {{cookiecutter.project_slug}}/database/docker-healthcheck.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# All credit for this file to docker-library/healthcheck on github
+set -eo pipefail
+
+host="$(hostname -i || echo '127.0.0.1')"
+user="${POSTGRES_USER:-postgres}"
+db="${POSTGRES_DB:-$POSTGRES_USER}"
+export PGPASSWORD="${POSTGRES_PASSWORD:-}"
+
+args=(
+	# force postgres to not use the local unix socket (test "external" connectibility)
+	--host "$host"
+	--username "$user"
+	--dbname "$db"
+	--quiet --no-align --tuples-only
+)
+
+if select="$(echo 'SELECT 1' | psql "${args[@]}")" && [ "$select" = '1' ]; then
+	exit 0
+fi
+
+exit 1
